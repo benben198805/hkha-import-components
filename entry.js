9c24e7c6a369b734d9a6e56af8bde4fd4b8da750
twomeetings1 <hhxu@thoughtworks.com>
2020-09-21 15:45:03 +0800

    init

---
+++ entry.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+const Container = ()=><div>React Web Application</div>
+
+ReactDOM.render(<Container></Container>, document.getElementById('root'))
+
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports={
+  entry:'./entry.js',
+  output:{
+    path: path.resolve(__dirname,'./dist'),
+    filename: './my-bundle.js'
+  },
+  module:{
+    loaders:[
+      { test:/\.jsx?$/, use:'babel-loader'}
+    ]
+  },
+  plugins:[ new HtmlWebpackPlugin({template:'./template.html'})]
+}
