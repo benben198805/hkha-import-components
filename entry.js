53beee3ce5db0db55458a00facb566b2472e9de5
twomeetings1 <hhxu@thoughtworks.com>
2020-09-21 16:29:11 +0800

    import components error

--- entry.js
+++ entry.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import { ShadowPanel } from 'workshop-hkha-components-test'
 
-const Container = ()=><div>React Web Application</div>
+const Container = ()=>
+  <div>
+    <div>React Web Application</div>
+    <ShadowPanel content='useShadowBox'></ShadowPanel>
+  </div>
 
 ReactDOM.render(<Container></Container>, document.getElementById('root'))
 
