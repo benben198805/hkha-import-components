cab46c446c483ac4ee5a5542c7c5ebc1bab80451
qlyan <qlyan@thoughtworks.com>
2021-07-28 00:36:30 +0800

    add hkha component

--- entry.js
+++ entry.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import { ShadowPanel } from 'workshop-create-components-hkha'
+import {ShadowPanel, CustomButton} from "hkha-react-component";
 
-const Container = ()=>
-  <div>
-    <h2>Other React Web Application - To Import Components</h2>
-    <ShadowPanel></ShadowPanel>
-  </div>
+const Container = () =>
+    <div>
+        <h2>Other React Web Application - To Import Components</h2>
+        <ShadowPanel></ShadowPanel>
+        <CustomButton text='test button' size='large'></CustomButton>
+    </div>
 
 ReactDOM.render(<Container></Container>, document.getElementById('root'))
 
