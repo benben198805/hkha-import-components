2744126ba08ed7112eb537bda2f16a43398bba50
qlyan <qlyan@thoughtworks.com>
2021-07-28 00:35:13 +0800

    format

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports={
-  entry:'./entry.js',
-  output:{
-    path: path.resolve(__dirname,'./dist'),
-    filename: './my-bundle.js'
-  },
-  module:{
-    rules:[
-      { test:/\.jsx?$/, use:'babel-loader'}
-    ]
-  },
-  plugins:[ new HtmlWebpackPlugin({template:'./template.html'})]
+module.exports = {
+    entry: './entry.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: './my-bundle.js'
+    },
+    module: {
+        rules: [
+            {test: /\.jsx?$/, use: 'babel-loader'}
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({template: './template.html'})]
 }
